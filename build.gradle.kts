f4cb88dd1812f5255477dd1139182a0563029914
matsudamper <let@matsudamper.net>
2025-06-24 04:08:00 +0900

    ktlintを導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,31 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktlingGradle) apply false
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<KtlintExtension> {
+        verbose.set(true)
+        version.set(
+            provider {
+                libs.versions.ktlint.get()
+            },
+        )
+        filter {
+            val excludePathList = listOf(
+                "generated",
+                "build",
+            ).map { "${File.separator}$it${File.separator}" }
+            exclude {
+                excludePathList.any { path -> it.file.path.contains(path) }
+            }
+        }
+    }
 }
