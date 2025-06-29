d8cb62b2d376d7b5df6cdeb26c4c8055a7fd6547
matsudamper <let@matsudamper.net>
2025-06-29 13:18:53 +0900

    format

--- app/src/main/java/net/matsudamper/allintoolscreensaver/compose/ScreenSaverScreen.kt
+++ app/src/main/java/net/matsudamper/allintoolscreensaver/compose/ScreenSaverScreen.kt
@@ -1,10 +1,6 @@
 package net.matsudamper.allintoolscreensaver.compose
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
