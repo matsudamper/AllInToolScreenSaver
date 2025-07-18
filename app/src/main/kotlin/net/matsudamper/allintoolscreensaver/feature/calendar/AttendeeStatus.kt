9e4156ef369902a66572a88d728f90c3df18789f
matsudamper <let@matsudamper.net>
2025-07-18 10:54:28 +0900

    add comment

--- app/src/main/kotlin/net/matsudamper/allintoolscreensaver/feature/calendar/AttendeeStatus.kt
+++ app/src/main/kotlin/net/matsudamper/allintoolscreensaver/feature/calendar/AttendeeStatus.kt
@@ -2,8 +2,24 @@ package net.matsudamper.allintoolscreensaver.feature.calendar
 
 enum class AttendeeStatus {
     UNKNOWN,
+
+    /**
+     * 承諾
+     */
     ACCEPTED,
+
+    /**
+     * 辞退
+     */
     DECLINED,
+
+    /**
+     *未回答
+     */
     INVITED,
+
+    /**
+     * 仮承諾
+     */
     TENTATIVE,
 }
