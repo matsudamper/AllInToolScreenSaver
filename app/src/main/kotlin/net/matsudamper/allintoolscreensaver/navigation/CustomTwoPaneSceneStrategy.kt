c855117981c3fda54497bd8a7e47c51d9b35131d
matsudamper <let@matsudamper.net>
2025-10-20 05:22:23 +0900

    format

--- app/src/main/kotlin/net/matsudamper/allintoolscreensaver/navigation/CustomTwoPaneSceneStrategy.kt
+++ app/src/main/kotlin/net/matsudamper/allintoolscreensaver/navigation/CustomTwoPaneSceneStrategy.kt
@@ -9,8 +9,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.NavEntry
-import androidx.navigation3.runtime.SceneStrategy
 import androidx.navigation3.runtime.Scene
+import androidx.navigation3.runtime.SceneStrategy
 import androidx.window.core.layout.WindowSizeClass
 
 class CustomTwoPaneSceneStrategy<T : Any> : SceneStrategy<T> {
